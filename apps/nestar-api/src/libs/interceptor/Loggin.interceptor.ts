084e8510adcd550b1633cbb3e9105c583fcfcad2
LEO <baxodirmamatvaliev@gmail.com>
2026-08-17 21:56:46 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/libs/interceptor/Loggin.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Loggin.interceptor.ts
@@ -1,18 +1,41 @@
 
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
+    private readonly logger: Logger = new Logger();
 
-    const now = Date.now();
-    return next
-      .handle()
-      .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)),
+  public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const recordTime = Date.now();
+    const requestType = context.getType<GqlContextType>(); // qanday req: kelaotganini blib olish uchun 
+    
+
+    if (requestType === "http") {
+    // ** Develop if needet //
+      return next.handle()
+    } else if ( requestType === "graphql") {
+        const gqContext = GqlExecutionContext.create(context);
+        console.log("gqContext =>",); 
+        this.logger.log(`${this.stringify(gqContext.getContext().req.body)}`,`Request`); 
+
+         // ** (2) Error handling via graphQL  *//
+     
+       
+      // ** (3) no Errors giving Response below  *//
+         
+     return next.handle().pipe(
+        tap((context) => {
+       const responseTime = Date.now() - recordTime;
+        this.logger.log(` ${this.stringify(context)} ${recordTime}ms \n\n`,`Request`);
+        }),
       );
+    } 
+    return next.handle();  
   }
+  private stringify(context: ExecutionContext): string {
+   return  JSON.stringify(context).slice(0, 75);
+  } 
 }
