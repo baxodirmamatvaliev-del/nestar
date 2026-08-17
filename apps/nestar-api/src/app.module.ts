c5b0caa069c3388b96da63e8442e2c85e9ace6a0
LEO <baxodirmamatvaliev@gmail.com>
2026-08-17 12:40:33 +0900

    feat: develop global graphQL error handling system

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -7,6 +7,7 @@ import { ApolloDriver } from '@nestjs/apollo'
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { T } from './libs/types/common';
 
 @Module({
   imports:
@@ -16,6 +17,15 @@ import { DatabaseModule } from './database/database.module';
     playground: true,
     uploads: false,
     autoSchemaFile: true,
+    formatError: (error: T) => {
+      const graphQLformattedError = {
+        code: error?.extensions.code,
+        message: error?.extensions?.excepton?.response?.message 
+        || error?.extensions?.response?.message || error?.message,
+      }
+      console.log("GRAPHQL GLOBALL ERROR!",graphQLformattedError)
+      return graphQLformattedError;
+    }
   }), 
   ComponentsModule, // asosiy mantiq yani modullar  
   DatabaseModule
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -21,8 +21,8 @@ constructor(@InjectModel("Member") private readonly memberModel: Model<Member>) 
    // TODO: Authentication via TOKKEN
    return result
    }catch(err) {
-    console.log("Error! Service.model",err)
-    throw new BadRequestException(err); 
+    console.log("Error! Service.model",err.message);
+    throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE); 
    }
    
   }
