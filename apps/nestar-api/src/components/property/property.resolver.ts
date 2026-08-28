6807b6fb7b20d7ee4ce039fffef1bc6e55b89dc3
LEO <baxodirmamatvaliev@gmail.com>
2026-08-28 19:29:45 +0900

    fear: develop getAllPropertiesByAdmin GpraphQL Aps

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -2,7 +2,7 @@
  import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { UseGuards } from '@nestjs/common';
@@ -74,6 +74,20 @@ public async getAgentProperties(
   console.log('Query: getAgentProperties');
   return await this.propertyService.getAgentProperties(memberId, input);
 }
-    
+
+
+/** ADMIN **/
+
+@Roles(MemberType.ADMIN)
+@UseGuards(RolesGuard)
+@Query((returns) => Properties)
+public async getAllPropertiesByAdmin(
+  @Args('input') input: AllPropertiesInquiry,
+  @AuthMember('_id') memberId: Types.ObjectId,
+): Promise<Properties> {
+  console.log('Query: getAllPropertiesByAdmin');
+  return await this.propertyService.getAllPropertiesByAdmin(input);
+}
+
 }
  
