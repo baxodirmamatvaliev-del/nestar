7f71b873188211b2ae8d96b364f0838b7484618c
LEO <baxodirmamatvaliev@gmail.com>
2026-09-01 11:06:30 +0900

    feat: develop removeCommentByAdmin GraphQL api

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -106,4 +106,10 @@ export class CommentService {
         if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
         return result[0];
     }
+
+    public async removeCommentByAdmin(commentId: Types.ObjectId): Promise<Comment> {
+        const result = await this.commentModel.findByIdAndDelete(commentId).exec();
+        if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+        return result;
+    }
 }
