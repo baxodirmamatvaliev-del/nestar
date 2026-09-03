a8be664635efec85230da4918c0168d07ffdcc75
LEO <baxodirmamatvaliev@gmail.com>
2026-09-03 11:05:43 +0900

    feat: create follow realated resolver and service model

---
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Follower, Followers } from '../../libs/dto/follow/follow';
+import { MemberService } from '../member/member.service';
+
+@Injectable()
+export class FollowService {
+    constructor(@InjectModel("Follow") private readonly followModel:Model<Follower | Followers>,
+    private readonly memberService: MemberService
+){}
+}
